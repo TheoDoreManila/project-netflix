147bb684b6c017afdaa3d1940c2654a7d507e22b
TheoDoreManila <33889457+TheoDoreManila@users.noreply.github.com>
2020-02-09 22:00:01 +1300

    fixed and finished

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const tabitems = document.querySelectorAll('.tab-item');
-const tabContentitems = document.querySelectorAll('.tab-content-item');
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
     removeBorder();
+    removeShow();
     // Add border to current tab
-    this.classlist.add('tab-border');
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
 }
 
 function removeBorder() {
     tabItems.forEach(item => item.classList.remove('tab-border'));
 }
 
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
 // Listen for tab click
-tabItems.foreach(item => item.addEventListener('click', selectItem));
+tabItems.forEach(item => item.addEventListener('click', selectItem));
